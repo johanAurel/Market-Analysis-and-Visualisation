0a6854e1ff7f876d8e649c5d351b162bcec8472f
root <aaurel.botouli@gmail.com>
2025-05-06 10:32:16 +0100

    modified command.sh and moved jenkinsfile to root of repo

--- code/command.sh
+++ code/command.sh
@@ -1,5 +1,8 @@
 # curl -X GET "https://api-fxpractice.oanda.com/v3/accounts/101-004-21322380-001" \
 # -H "Authorization: Bearer bb1d421d5ff8b46afc88b98ea098fb16-6366a971f49832b64f2992233bb88b6b"
+docker rm jenkins-container
+docker rmi jenkins/jenkins:lts-alpine
 echo "starting a jenkins container as docker image on port 8080"
 docker run -d --name jenkins-container -p 8080:8080 jenkins/jenkins:lts-alpine
-
+echo "jenkins adminPassword :"
+docker exec -it jenkins-container cat /var/jenkins_home/secrets/initialAdminPassword
